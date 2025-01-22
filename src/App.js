7bd4ccc91e094e1dbb29bcc545bff927f862bbf8
Wes Mitchell <wmdrums@gmail.com>
2025-01-22 11:12:30 -0600

    initial setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import './index.css';
+import About from "./routes/About"
+import Contact from "./routes/Contact"
+import Home from "./routes/Home"
+import Project from "./routes/Project"
+
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/project" element={<Project/>}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
