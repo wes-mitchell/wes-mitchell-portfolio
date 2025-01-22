0dce921b3125246a3f9ddb5b6e694b57417ecd95
Wes Mitchell <wmdrums@gmail.com>
2025-01-22 11:24:37 -0600

    prettier config

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import './index.css';
-import About from "./routes/About"
-import Contact from "./routes/Contact"
-import Home from "./routes/Home"
-import Project from "./routes/Project"
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Home from './routes/Home';
+import Project from './routes/Project';
 
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/project" element={<Project/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/project" element={<Project />} />
       </Routes>
     </>
   );
