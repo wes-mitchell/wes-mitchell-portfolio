d1b48313ef1a78a2e24c3864b3ee69d9564dc98d
Wes Mitchell <wmdrums@gmail.com>
2025-01-27 14:40:25 -0600

    base path in public folder

--- src/data/WebLinks.js
+++ src/data/WebLinks.js
@@ -2,6 +2,7 @@ const WebLinks = {
   githubProfile: 'https://github.com/wes-mitchell',
   linkedInProfile: 'https://www.linkedin.com/in/wesleymitchell87/',
   wmdrums: 'https://www.wmdrums.com/',
+  portfolio: 'https://wes-mitchell.github.io/wes-mitchell-portfolio/',
 };
 
 export default WebLinks;
