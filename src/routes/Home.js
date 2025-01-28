f7b77ca842174b47ccba1203ed74adaa283e7563
Wes Mitchell <wmdrums@gmail.com>
2025-01-28 13:56:40 -0600

    unused import for deploy

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import HeroImage from '../components/HeroImage';
 import Footer from '../components/Footer';
-import Work from 'components/Work';
 
 const Home = () => {
   return (
